709bde6074e584891d74dd084056b6755bea226b
Miracles_1000th <13305583+miracles-1000th@user.noreply.gitee.com>
2025-10-12 20:28:11 +0800

    cont

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Arial', sans-serif;
+  line-height: 1.6;
+}
+
+#app {
+  min-height: 100vh;
+}
+
+</style>
